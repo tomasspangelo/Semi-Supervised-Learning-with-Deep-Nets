7f3836144546a32a7a609c5091a5ee09633c9fa0
Tomas Haugland Spangelo <tomas@spangelo.com>
2021-03-31 16:33:42 +0200

    Implemented classifier and classifier head

--- classifier.py
+++ classifier.py
@@ -1,26 +1,34 @@
 import tensorflow as tf
+from tensorflow.keras.layers import Dense, Softmax
 
 
 class Classifier(tf.keras.Model):
-    def __init__(self, encoder, classifier_head, name="classifier", **kwargs):
+    def __init__(self, encoder, name="classifier", **kwargs):
         super(Classifier, self).__init__(name=name, **kwargs)
         self.encoder = encoder
-        self.classifier_head = classifier_head
+        self.classifier_head = ClassifierHead()
 
     def call(self, inputs, training=None, mask=None):
-        return
+        x = self.encoder(inputs)
+        return self.classifier_head(x)
 
     def get_config(self):
         raise NotImplementedError("Not implemented")
 
 
 class ClassifierHead(tf.keras.Model):
-    def __init__(self, name="classifierhead", **kwargs):
-        super(Classifier, self).__init__(name=name, **kwargs)
+    def __init__(self, num_classes, name="classifierhead", **kwargs):
+        super(ClassifierHead, self).__init__(name=name, **kwargs)
+        self.dense1 = Dense(60, activation="tanh")
+        self.dense2 = Dense(20, activation="tanh")
+        self.dense3 = Dense(num_classes, activation="sigmoid")
+        self.softmax = Softmax()
 
     def call(self, inputs, training=None, mask=None):
-        return
+        x = self.dense1(inputs)
+        x = self.dense2(x)
+        x = self.dense3(x)
+        return self.softmax(x)
 
     def get_config(self):
         raise NotImplementedError("Not implemented")
-
