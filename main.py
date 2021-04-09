401d55ea79a7aa950829b4d4c95c48110a7d698a
Tomas Haugland Spangelo <tomas@spangelo.com>
2021-04-09 13:42:28 +0200

    Added comments

--- main.py
+++ main.py
@@ -180,6 +180,8 @@ def init_classifier(classifier_config, encoder, num_classes, freeze):
 
 def main():
     """Main method for the system."""
+
+    # Check if config file argument is provided.
     if len(sys.argv) < 2:
         print("Please indicate config file from /Config and try again.")
         return
@@ -190,23 +192,28 @@ def main():
     vis_config = config["visualization"]
     plot_tsne = vis_config.getboolean("tSNE")
 
+    # Initialize the data
     data_config = config["dataset"]
     d1, d2, num_classes, input_shape = init_data(data_config)
     x_d1_train, x_d1_val, y_d1_train, y_d1_val = d1
     x_d2_train, x_d2_val, x_d2_test, y_d2_train, y_d2_val, y_d2_test = d2
 
+    # Initialize the autoencoder
     ae_config = config["autoencoder"]
     autoencoder = init_autoencoder(ae_config, input_shape)  # Should not be used
 
+    # Initialize semi-supervised learner
     classifier_config = config["classifier"]
     freeze = classifier_config.getboolean("freeze")
 
     ssl = SSL(autoencoder=autoencoder,
               classifier=init_classifier(classifier_config, autoencoder.encoder, num_classes, freeze))
 
+    # Plot tSNE before any training
     if plot_tsne:
         tsne_fig1 = tsne(x_d1_train[:200], y_d1_train[:200], 1, ssl.get_encoder())
 
+    # Train autoencoder-part of semi-supervised learner
     epochs = int(ae_config["epochs"])
     batch_size = int(ae_config["batch_size"])
     print("Training autoencoder:")
@@ -215,13 +222,16 @@ def main():
                                   batch_size=batch_size,
                                   x_val=x_d1_val)
 
+    # Plot tSNE after autoencoder training
     if plot_tsne:
         tsne_fig2 = tsne(x_d1_train[:200], y_d1_train[:200], 2, ssl.get_encoder())
 
+    # Plot reconstructions
     reconstructions = int(vis_config["reconstructions"])
     ImageViewer.view(x_d1_train[:reconstructions], n_cols=4)
     ImageViewer.view(ssl.forward_ae(x_d1_train[:reconstructions]), n_cols=4)
 
+    # Plot autoencoder loss
     fig = plt.figure()
     plt.plot(ae_hist.history["loss"], 'b', label="loss")
     plt.plot(ae_hist.history["val_loss"], 'g', label="val_loss")
@@ -230,6 +240,7 @@ def main():
     plt.legend(loc="upper right")
     fig.show()
 
+    # Train classifier part of semi-supervised learner
     epochs = int(classifier_config["epochs"])
     batch_size = int(classifier_config["batch_size"])
 
@@ -242,9 +253,11 @@ def main():
     print("Evaluating C1 on D2 test set:")
     ssl.evaluate_classifier(x_d2_test, y_d2_test)
 
+    # Initialize purely supervised classifier
     latent_size = ae_config["latent_size"]
     classifier2 = init_classifier(classifier_config, Encoder(latent_size), num_classes, freeze)
 
+    # Train purely supervised classifier
     print("Training C2:")
     hist2 = classifier2.fit(x_d2_train,
                             y_d2_train,
@@ -260,6 +273,7 @@ def main():
     print("Evaluating C2 on D1 training set:")
     classifier2.evaluate(x_d1_train, y_d1_train)
 
+    # Plot classifier (both semi and supervised) accuracy from training
     fig = plt.figure()
     plt.plot(hist1.history["accuracy"], 'b', label="Semi-accuracy")
     plt.plot(hist1.history["val_accuracy"], 'orange', label="Semi-val_accuracy")
@@ -270,6 +284,8 @@ def main():
     plt.legend(loc="lower right")
     fig.show()
 
+    # Plot tSNE after semi-supervised learner has trained both autoencoder
+    # and classifier.
     if plot_tsne:
         tsne_fig3 = tsne(x_d1_train[:200], y_d1_train[:200], 3, ssl.get_encoder())
         tsne_fig1.show()
